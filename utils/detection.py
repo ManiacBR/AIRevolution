20f6591e192126d4120f233f2dd813a54882efad
ManiacBR <66705599+ManiacBR@users.noreply.github.com>
2025-04-19 11:27:51 -0300

    Delete utils directory

--- utils/detection.py
+++
@@ -1,7 +0,0 @@
-async def is_message_to_bot(message, bot_user):
-    content = message.content.lower()
-    if bot_user.mentioned_in(message):
-        return True
-    if "revolution" in content or "ai revolution" in content:
-        return True
-    return False
