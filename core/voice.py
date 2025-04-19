22f1c24aa8f6f0045b96f7f6d3636bd4e9dda068
ManiacBR <66705599+ManiacBR@users.noreply.github.com>
2025-04-19 11:27:44 -0300

    Delete core directory

--- core/voice.py
+++
@@ -1,6 +0,0 @@
-import edge_tts
-import asyncio
-
-async def text_to_speech(text, filename="resposta.mp3"):
-    communicate = edge_tts.Communicate(text, "pt-BR-AntonioNeural")
-    await communicate.save(filename)
